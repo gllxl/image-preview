a1ec16d6d75aee2bb6244d9e81dd48032728665a
gllxl <gllxl@outlook.com>
2023-03-13 23:13:36 +0800

    chore: optimize image link judgment

--- src/main/kotlin/com/github/gllxl/imagepreview/Utils.kt
+++ src/main/kotlin/com/github/gllxl/imagepreview/Utils.kt
@@ -6,6 +6,7 @@ fun getVariableContent (element: PsiElement): String {
   return element.node.lastChildNode.text.replace("\'", "").replace("\"", "")
 }
 
-fun isImageUrl (url: String): Boolean {
-  return url.startsWith("https://") && url.endsWith(".png")
+fun isImageUrl(url: String): Boolean {
+  val regex = "^(http|https)://.*(jpg|jpeg|png|gif|bmp)$".toRegex(RegexOption.IGNORE_CASE)
+  return regex.containsMatchIn(url)
 }
--- src/test/kotlin/com/github/gllxl/imagepreview/Utils.kt
+++ src/test/kotlin/com/github/gllxl/imagepreview/Utils.kt
@@ -7,8 +7,14 @@ import com.intellij.testFramework.fixtures.BasePlatformTestCase
 class Utils : BasePlatformTestCase() {
 
     fun testIsImageUrl() {
-        val imageUrl = "https://g.alicdn.com/buc-fe/sso-civil/1.19.0/assets/931f81d3d9d65243c35f0a481bd2872a.png"
+        val pngImageUrl = "https://g.alicdn.com/buc-fe/sso-civil/1.19.0/assets/931f81d3d9d65243c35f0a481bd2872a.png"
+        val jpgImageUrl = "https://img.zcool.cn/community/0147cb5f58366211013e3187e8c713.jpg"
+        val httpImageUrl = "http://g.alicdn.com/buc-fe/sso-civil/1.19.0/assets/931f81d3d9d65243c35f0a481bd2872a.png"
+        val noImageUrl = "//g.alicdn.com/buc-fe/sso-civil/1.19.0/assets/931f81d3d9d65243c35f0a481bd2872a.png"
 
-        assertEquals(true, isImageUrl(imageUrl))
+        assertEquals(true, isImageUrl(pngImageUrl))
+        assertEquals(true, isImageUrl(jpgImageUrl))
+        assertEquals(true, isImageUrl(httpImageUrl))
+        assertEquals(false, isImageUrl(noImageUrl))
     }
 }
